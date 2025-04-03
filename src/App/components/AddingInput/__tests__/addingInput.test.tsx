c964da7562ce7a2ab38751e0b46be2d9cb2648b2
Glazunov Andey <andrew.glaz@yandex.ru>
2025-04-03 21:33:38 +0300

    tests: add RTL tests for AddingInput and add rtl/user-event

--- src/App/components/AddingInput/__tests__/addingInput.test.tsx
+++ src/App/components/AddingInput/__tests__/addingInput.test.tsx
@@ -1,6 +1,41 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
 import { AddingInput } from '../AddingInput'
 
-it('component "AddingInput" should be rendered', () => {
-    render(<AddingInput onAddItem={jest.fn()} />)
+describe('testing "AddingInput" component', () => {
+    const user = userEvent.setup()
+    const mockedAddItem = jest.fn()
+
+    afterEach(() => {
+        mockedAddItem.mockClear()
+    })
+
+    it('adding a new empty element', async () => {
+        render(<AddingInput onAddItem={mockedAddItem} />)
+        const button = screen.getByRole('button', { name: 'Add item' })
+
+        await user.click(button)
+
+        expect(mockedAddItem).toHaveBeenCalledWith('')
+    })
+
+    it('adding a new element by pressing a button', async () => {
+        render(<AddingInput onAddItem={mockedAddItem} />)
+        const input = screen.getByPlaceholderText('Enter new TODO item')
+        const button = screen.getByRole('button', { name: 'Add item' })
+
+        await user.type(input, 'Test item')
+        await user.click(button)
+
+        expect(mockedAddItem).toHaveBeenCalledWith('Test item')
+    })
+
+    it('adding a new element by pressing enter', async () => {
+        render(<AddingInput onAddItem={mockedAddItem} />)
+        const input = screen.getByPlaceholderText('Enter new TODO item')
+
+        await user.type(input, 'Test item{Enter}')
+
+        expect(mockedAddItem).toHaveBeenCalledWith('Test item')
+    })
 })
